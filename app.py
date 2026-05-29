8571d503e426dfbf08301940753675ec05e861f7
maidjc <2407963475@qq.com>
2026-05-29 10:55:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from openai import APIError, APIConnectionError, RateLimitError
 try:
     API_KEY = st.secrets["API_KEY"]
 except:
-    API_KEY = ""
+    API_KEY = "4279ab216a1e4c8282b51f541aff703e.HJdsPUVWqGbMD7t0"
 
 BASE_URL = "https://open.bigmodel.cn/api/paas/v4/"
 MODEL_NAME = "glm-4-flash"
